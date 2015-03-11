509b9d6eb5319e7eb651bec053b6f94f0b3eb6a4
Daria <dasham94@yandex.ru>
2015-03-12 01:04:40 +0300

    VM 01(без стека) Итерационный метод вычисления

--- src/VirtualMachine.java
+++ src/VirtualMachine.java
@@ -8,117 +8,155 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Vector;
 
 public class VirtualMachine {
 
-    private ByteBuffer[] memoryProgram = new ByteBuffer[6];
-    HashMap< Integer, String > commandsList;
-    //private int startPointOfMain;
-    private short recursionDepth = -1;
-    private ArrayList<Byte> callStack = new ArrayList<>();
-    private Byte carriagePos;
+    private ArrayList<Vector<Integer> > stackMemoryVar = new ArrayList<>();
+    private ArrayList<Integer> memoryConst;
+    private ByteBuffer byteCode;
+    private Byte offsetConstMemory;
+    private Byte offsetMain;
+    private  Vector<ArrayList<Byte>> garbageCollector = new Vector<>();
+    Byte carriagePos;
+    Vector<Byte> callStack = new Vector<>();
+    Scanner input;
 
     VirtualMachine() {
-        commandsList = new HashMap<Integer, String>();
-        commandsList.put(0, "input");
-        commandsList.put(1, "output");
-        commandsList.put(2, "add");//ответ, арг, арг
-        commandsList.put(3, "move");//ответ арг
-        commandsList.put(4, "isEqual");//ответ, арг, арг
-        commandsList.put(5, "ifTrueThenElse");
-        commandsList.put(6, "enterToNewFunction"); //арг.: startPoint функции, ссылку на место, куда пишем результат, аргумент
-                                                   // кладём в callStack позицию, следующую за местом вызова,
-                                                   // а затем ссылку вызова+2, т.к. 2 байта - на внутренние парметры()
-        commandsList.put(7, "exitFromCurrentFunction");//записать ответ и снять
-                                                       // со стека вызовов последний startPoint
-        commandsList.put(8, "deduct");
+        input = new Scanner(System.in);
+    }
+
+    private void incOffset(int i) {
+        for (int j = 0; j < i; ++j) {
+            ++carriagePos;
+        }
+    }
+    void read(String aFileName) throws IOException {
+        Path path = Paths.get(aFileName);
+        //считали бинарник
+        byteCode = ByteBuffer.wrap(Files.readAllBytes(path));
+    }
 
 
 
+    private void inputVal() throws Exception {
+        ++carriagePos;
+        byte address = byteCode.get(carriagePos);
+        incOffset(3);
+        Integer arg = input.nextInt();
+        byteCode.putInt(address, arg);
+    }
 
+    private void ifLess() {
+        ++carriagePos;
+        byte addressFirstArg = byteCode.get(carriagePos);
+        ++carriagePos;
+        byte addressSecondArg = byteCode.get(carriagePos);
+        if (byteCode.getInt(addressFirstArg) < byteCode.getInt(addressSecondArg)) {
+            incOffset(2);
+        } else {
+            ++carriagePos;
+            byte addressForJump = byteCode.get(carriagePos);
+            carriagePos = addressForJump;
+        }
+    }
+
+    private void goTo() {
+        ++carriagePos;
+        byte addressForJump = byteCode.get(carriagePos);
+        carriagePos = addressForJump;
+    }
+
+    private void deduct() {
+        ++carriagePos;
+        byte addressOfResult = byteCode.get(carriagePos);
+        ++carriagePos;
+        Integer theFirstArg = byteCode.getInt(byteCode.get(carriagePos));
+        ++carriagePos;
+        Integer theSecondArg = byteCode.getInt(byteCode.get(carriagePos));
+        byteCode.putInt(addressOfResult, theFirstArg - theSecondArg);
+        ++carriagePos;
+
+    }
+
+    void mov() {
+        ++carriagePos;
+        byte addressOfResult = byteCode.get(carriagePos);
+        ++carriagePos;
+        byte addressOfArgument = byteCode.get(carriagePos);
+        incOffset(2);
+        Integer argument = byteCode.getInt(addressOfArgument);
+        byteCode.putInt(addressOfResult, argument);
     }
 
     void output() {
         ++carriagePos;
-        byte addressOfResult = memoryProgram[0].get(carriagePos);
-        Integer argument = memoryProgram[recursionDepth].getInt(addressOfResult);
+        byte addressOfResult = byteCode.get(carriagePos);
+        Integer argument = byteCode.getInt(addressOfResult);
         System.out.println(argument);
-        ++carriagePos;
+        incOffset(3);
     }
 
-    void add() {
+    void add() {//ответ, арг, арг
         ++carriagePos;
-        byte addressOfResult = memoryProgram[0].get(carriagePos);
+        byte addressOfResult = byteCode.get(carriagePos);
         ++carriagePos;
-        Integer theFirstArg = memoryProgram[recursionDepth].getInt(memoryProgram[recursionDepth].get(carriagePos));
+        Integer theFirstArg = byteCode.getInt(byteCode.get(carriagePos));
         ++carriagePos;
-        Integer theSecondArg = memoryProgram[recursionDepth].getInt(memoryProgram[recursionDepth].get(carriagePos));
-        memoryProgram[recursionDepth].putInt(addressOfResult, theFirstArg+theSecondArg);
+        Integer theSecondArg = byteCode.getInt(byteCode.get(carriagePos));
+        byteCode.putInt(addressOfResult, theFirstArg + theSecondArg);
         ++carriagePos;
     }
-
-    void enterToNewFunction() {
-        Integer pointReturn = carriagePos.intValue()+4;
-        callStack.add(pointReturn.byteValue());//  позиция по возвращении
-        ++carriagePos;
-        Byte startPoint = memoryProgram[0].get(carriagePos);
-        callStack.add(startPoint); //точка входа новой функции
-        ++carriagePos;
-      //  Byte argumentAdress =
-        //callStack.add()
-    }
-
-    void read(String aFileName) throws IOException {
-        Path path = Paths.get(aFileName);
-        //считали бинарник в память
-        memoryProgram[0] = ByteBuffer.wrap(Files.readAllBytes(path));
-        System.out.println(memoryProgram[0].toString());
-        //считываем адресс начала действующего кода
-        Integer startPointOfMain = memoryProgram[0].getInt();
-        //блок памяти, отвечающий за переменные, размножаем до максимальной глубины рекурсии (у нас - 5)
-        System.out.println(startPointOfMain);
-        byte[] bufOfValueBlock = new byte[startPointOfMain - 4];
-        System.out.println(bufOfValueBlock.length);
-        memoryProgram[0].get(bufOfValueBlock, 0, startPointOfMain - 4);
-        for (int i = 1; i < 6; ++i) {
-            memoryProgram[i] = ByteBuffer.allocate(startPointOfMain - 4);
-            memoryProgram[i].put(bufOfValueBlock);
-        }
-    }
-
-    Integer getNumber(int address) {
-        //ByteBuffer.getInt(int index) - Reads four bytes at the given index, composing them
-        // into a int value according to the current byte order:
-        return  memoryProgram[recursionDepth].getInt(address);
-    }
-
-
-
-    void execute() {
-        Integer carriagePos = memoryProgram[0].getInt(0);
-        Integer length = memoryProgram[0].array().length;
+    
+    void execute() throws Exception {
+        carriagePos = byteCode.get();
+        Integer length = byteCode.array().length;
         while (carriagePos < length) {
-            Byte commandCode = memoryProgram[0].get(carriagePos);
+            Byte commandCode = byteCode.get(carriagePos);
             byte[] args = new byte[3];
             switch (commandCode) {
-                case 2:
+                case 1:
                 {
-                    add();
+                    inputVal();
                     break;
                 }
-                case 1:
+                case 2:
                 {
                     output();
                     break;
                 }
-                case 6:
+                case 3:
                 {
-                    enterToNewFunction();
+                    mov();
                     break;
                 }
+                case 4:
+                {
+                    add();
+                    break;
+                }
+                case 5:
+                {
+                    deduct();
+                    break;
+                }
+                case 6:
+                {
+                    goTo();
+                    break;
+                }
+                case 7:
+                {
+                    ifLess();
+                    break;
+                }
+                case 8:
+                    System.exit(0);
+                default:
+                    incOffset(3);
+                    break;
             }
         }
     }
-
 }
